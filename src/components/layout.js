ea67ade030f461679fb9fa0366915d67bb5f9ec0
Ace Alrich <hex0@live.com>
2020-10-29 22:27:36 +0000

    split blog

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,19 +6,13 @@ const Layout = ({ location, title, children }) => {
   const isRootPath = location.pathname === rootPath
   let header
 
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
+  
     header = (
       <Link className="header-link-home" to="/">
         {title}
       </Link>
     )
-  }
+  
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
