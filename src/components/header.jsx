9222e87980192634e4e0c2acac53e16a30578ee1
Ace Alrich <hex0@live.com>
2020-10-30 07:51:56 +0000

    add fonts

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,20 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
 const Header = () => {
   return (
-      <header className="global-header"> 
+    <header className="global-header">
+      <Link to="/">HOME</Link>
+      <Link to="/">ABOUT</Link>
+      <Link to="/">PRICING</Link>
       <Link to="/">
-        HOME
+        <span>VITA</span>
       </Link>
-      <Link to="/">
-        ABOUT
-      </Link>
-      <Link to="/">
-        PRICING
-      </Link>
-      <Link to="/">
-        GALLERY
-      </Link>
-      <Link to="/">
-        BLOG
-      </Link>
-      <Link to="/">
-        CONTACT
-      </Link>
-      </header>
-  )
-}
+      <Link to="/">GALLERY</Link>
+      <Link to="/">BLOG</Link>
+      <Link to="/">CONTACT</Link>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
