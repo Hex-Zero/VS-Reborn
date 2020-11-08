253428134844e2d03e8ffc75bf40acd39873a289
Ace Alrich <hex0@live.com>
2020-11-08 07:31:59 +0000

    add images

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,18 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Available options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `webp`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+        failOnError: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
