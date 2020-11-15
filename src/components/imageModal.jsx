e071d831cb302c4f2e5e4b2480f31ea38fc728e9
Ace Alrich <hex0@live.com>
2020-11-15 02:44:22 +0000

    pictures show up

--- src/components/imageModal.jsx
+++ src/components/imageModal.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { Link } from "gatsby";
+import React, { useEffect, useState } from "react";
+import Img from "gatsby-image";
 
-const ImageModal = ({ imageIndex }) => {
+const ImageModal = ({ imageIndex, imageArray }) => {
+  const [showModal, setShowModal] = useState("");
+  useEffect(() => {
+    if (!imageIndex.index) {
+      setShowModal("");
+    } else {
+      setShowModal("max-show");
+    }
+  }, [imageIndex]);
+
   return (
     <>
-      <div className="max-image-modal-wrapper">
-        <div className="max-carousel"></div>
+      <div
+        onClick={(e) => setShowModal("")}
+        className={"max-image-modal-wrapper " + showModal}
+      >
+        <div className="max-carousel">
+          <Img
+            fluid={imageArray[imageIndex.index - 1].node.childImageSharp.fluid}
+          />
+        </div>
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import React from "react"
+import React, {useState } from "react"
 import { graphql } from "gatsby"
 import Img from 'gatsby-image'
 
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ImageModal from "../components/imageModal"
 
 function returnLayoutSize(width, height) {
-  console.log(width, height);
   if (width > height) {
     return "max-two-column-wide"
   } else if (width == height) {
@@ -17,17 +17,21 @@ function returnLayoutSize(width, height) {
   }
 }
 
-const BlogIndex = (props) => {
-  const siteTitle = props.data.site.siteMetadata?.title || `Title`
-  const posts = props.data.allMarkdownRemark.nodes
+const BlogIndex = ({data, location}) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const [imageIndex, setImageIndex] = useState({event: {}, index: 0})
+ 
+
   return (
-    <Layout location={props.location} title={siteTitle}>
+    <Layout location={location} title={siteTitle}>
       <SEO title="HOME" />
+      <ImageModal imageIndex={imageIndex} imageArray={data.allFile.edges}></ImageModal>
       <section className="max-main-image-wrapper">
-        {props.data.allFile.edges.map((element, index) => {
-          const layout = returnLayoutSize(element.node.childImageSharp.original.width, element.node.childImageSharp.original.height)
-          return <Img key={index} className={"max-main-image " + layout}
-            fluid={element.node.childImageSharp.fluid} />
+        {data.allFile.edges.map((element, index) => {
+          const sharpChild = element.node.childImageSharp;
+          const layout = returnLayoutSize(sharpChild.original.width, sharpChild.original.height)
+          return <div key={index} onClick={e=>setImageIndex({event:e, index: index+1})} className={"max-main-image " + layout} ><Img 
+            fluid={sharpChild.fluid}/></div>
         })}
         </section>
     </Layout>
