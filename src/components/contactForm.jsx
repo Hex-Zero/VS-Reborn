97741bf8f6cf8d585950fde848d16c646a6ae387
Ace Alrich <hex0@live.com>
2020-12-04 23:58:10 +0000

    for

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -12,12 +12,14 @@ const ContackForm = () => {
       data-netlify-honeypot="bot-field"
     >
       <input type="hidden" name="form-name" value="contact" />
-      <label htmlFor="max-contact-name">Name</label>
-      <input id="max-contact-name" type="text" name="name" />
-      <label htmlFor="max-contact-email">E-mail</label>
-      <input id="max-contact-email" type="email" name="email" />
-      <label htmlFor="max-contact-content">Subject</label>
-      <textarea id="max-contact-content" name="message"></textarea>
+      <div className="max-contact-form-container">
+        <label htmlFor="max-contact-name">Name</label>
+        <input id="max-contact-name" type="text" name="name" />
+        <label htmlFor="max-contact-email">E-mail</label>
+        <input id="max-contact-email" type="email" name="email" />
+        <label htmlFor="max-contact-content">Subject</label>
+        <textarea id="max-contact-content" name="message"></textarea>
+      </div>
     </form>
   );
 };
