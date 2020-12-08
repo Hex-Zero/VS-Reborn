0516c01f44a459e5196f7fa1a15a889c7b8be336
Ace Alrich <hex0@live.com>
2020-12-08 20:41:41 +0000

    remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sharp`,
       options: {
-        // Available options and their defaults:
         base64Width: 20,
         forceBase64Format: `webp`, // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
